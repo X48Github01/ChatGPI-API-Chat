2fd89d4fc229cf4c142ce2a253c2ded1d5c04866
X48Github01 <101425462+X48Github01@users.noreply.github.com>
2023-01-05 16:29:05 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
+import openai
 import random
 from configparser import ConfigParser
-
-import colorama
-import openai
-from colorama import Back, Fore, Style, init
 from rich.console import Console
-
+import colorama
+from colorama import Fore, Back, Style, init
 init()
 
 console = Console()
@@ -13,16 +11,39 @@ console = Console()
 dbconf = ConfigParser()
 dbconf.read_file(open("config.ini"))
 
-# Set the OpenAI API key
-openai.api_key = dbconf.get("SETTING", "API_KEY")
-print("\n")
+print("")
+print(Fore.LIGHTGREEN_EX, Style.BRIGHT + "Welcome ChatGPT-OpenAI by X48-Python.EXE\n" + Style.RESET_ALL)
+print(Fore.LIGHTMAGENTA_EX, Style.BRIGHT + "This is a little program to help someone use ChatGPT without web browser\n" + Style.RESET_ALL)
+promptpay_donate = 'Buy Me a Coffee : PromptPay : 095-518-8528'
+print(Fore.LIGHTYELLOW_EX, Style.BRIGHT + promptpay_donate + Style.RESET_ALL)
+print(Fore.LIGHTGREEN_EX, Style.BRIGHT + 'Thanks For Support' + Style.RESET_ALL)
+print('---------------------------------------------------------------------------------')
+print('██   ██ ██   ██  █████   ██ ███████  ██  ██████  ██████  ██████  ██   ██ ██████ ')
+print(' ██ ██  ██   ██ ██   ██ ███ ██      ███ ██            ██      ██ ██   ██      ██')
+print('  ███   ███████  █████   ██ ███████  ██ ███████   █████   █████  ███████  █████ ')
+print(' ██ ██       ██ ██   ██  ██      ██  ██ ██    ██ ██           ██      ██ ██     ')
+print('██   ██      ██  █████   ██ ███████  ██  ██████  ███████ ██████       ██ ███████')
+print('---------------------------------------------------------------------------------')
+print(Fore.YELLOW + '')
+print(" ██████╗██╗  ██╗ █████╗ ████████╗    ██████╗ ██████╗ ████████╗")
+print("██╔════╝██║  ██║██╔══██╗╚══██╔══╝   ██╔════╝ ██╔══██╗╚══██╔══╝")
+print("██║     ███████║███████║   ██║█████╗██║  ███╗██████╔╝   ██║   ")
+print("██║     ██╔══██║██╔══██║   ██║╚════╝██║   ██║██╔═══╝    ██║   ")
+print("╚██████╗██║  ██║██║  ██║   ██║      ╚██████╔╝██║        ██║   ")
+print(" ╚═════╝╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝       ╚═════╝ ╚═╝        ╚═╝   ")
+print(Style.RESET_ALL + '')
+print('---------------------------------------------------------------------------------')
+print(Fore.BLUE, Style.BRIGHT + 'If You Found Some Bug Please Contact My Developer Master >> Here Line ID : x4815x')
+print(Style.RESET_ALL)
 
+# Set the OpenAI API key
+openai.api_key = dbconf.get("SETTING","API_KEY")
+#print("\n")
 
 def main():
     # Set the prompt that the ChatGPT model will use to generate responses
-    prompt = console.input(
-        "\n[bold white3]Enter TEXT Prompt (use '\ n' for other prompt) : \n"
-    )
+    prompt = console.input("\n[bold white3]>>> Enter TEXT Prompt or Some Question : \n")
+    print(Fore.LIGHTBLUE_EX, Style.DIM + "\nPlease Wait The Anwser >>>")
 
     # Use the OpenAI `Completion` API to generate responses to the prompt
     response = openai.Completion.create(
@@ -34,10 +55,16 @@ def main():
 
     # Print the generated responses
     for i, response in enumerate(response.choices[0].text.split("\n")):
-        print(Fore.LIGHTGREEN_EX, Style.BRIGHT, f"{response}", Style.RESET_ALL)
-
+        print(Fore.LIGHTMAGENTA_EX, Style.BRIGHT + "")
+        print(f"{response}")
+    #print
 
 if __name__ == "__main__":
     while True:
-        main()
+        try:
+            main()
+            print(Style.RESET_ALL)
+        except:
+            print(Fore.YELLOW, Style.BRIGHT, Back.RED + "Master !!!!! Please Check API Form OpenAi Website and Try Again" + Style.RESET_ALL)
+            print(Style.RESET_ALL)
         continue
